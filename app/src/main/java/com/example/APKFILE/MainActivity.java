f3a303f7b24a557dadf8c5fcaeab5018e651851b
Sohel <sohelsp9730@gmail.com>
2022-04-12 10:25:17 +0530

    second commit

--- app/src/main/java/com/example/APKFILE/MainActivity.java
+++ app/src/main/java/com/example/APKFILE/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.APKFILE;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+    // On click Listener function
+    public void openActivity(View v){
+        Toast.makeText(this, "Login ", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,login.class);
+        startActivity(intent);
     }
 }
